73458101b80e6cb88cad3ff1db1873117b2b2cc5
JanderC <janderalexisc@gmail.com>
2025-12-05 15:50:33 -0400

    cambios en el servidor de la base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '200.40.68.122',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'vidafit',
+  database: process.env.DB_NAME || 'respaldo3',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'admin',
+  password: process.env.DB_PASSWORD || 'p4ng34t3ch',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
