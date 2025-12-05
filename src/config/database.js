8ccb02de430e1402052d9157274579234b8ee308
JanderC <janderalexisc@gmail.com>
2025-12-05 15:44:35 -0400

    cambios

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'vida_fit_db',
+  database: process.env.DB_NAME || 'vidafit',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'admin',
   max: 20,
